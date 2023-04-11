34eb5a815a7e7e36f4c1356a875526c013e71f9c
Alexander Vasilchenko <sanloid2@gmail.com>
2023-04-12 01:18:35 +0600

    cors...

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://127.0.0.1:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     // credentials: true,
   });
